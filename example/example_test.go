90fd05a4f783c03d0539d47d737a4b90af66aca9
sinlov <sinlovgmppt@gmail.com>
2025-03-10 14:38:59 +0800

    feat(example): add config.yml and update example_test.go
    - Add config.yml file for logger configuration
    - Update example_test.go to use viper for config loading
    - Initialize logger with config from config.yml
    - Add log output in example_test.go

--- example/example_test.go
+++ example/example_test.go
@@ -1,27 +1,33 @@
 package example
 
 import (
-	"fmt"
+	"github.com/sinlov-go/zlog-zap-wrapper/zlog"
+	"github.com/spf13/viper"
 )
 
-// To use this lib package, just
+// To use this lib, can load by viper
 // the SetOutput function when your application starts.
 func Example() {
-	// Basic usage of this lib
-	fooOut := Foo(1, 2)
+	// load viper
+	viper.SetConfigType("yml")
+	viper.SetConfigFile("config.yml")
 
-	fmt.Printf("fooOut %v\n", fooOut)
-	// Output:
-	// fooOut 3
-}
+	errViperReadInConfig := viper.ReadInConfig()
+	if errViperReadInConfig != nil {
+		panic(errViperReadInConfig)
+	}
 
-// other different examples such as bar naming can be written like this
-// When parsing, the first letter of the Example_ content will be automatically capitalized.
-func Example_bar() {
-	// For some usage of example.Foo
-	barOut := Foo(3, 4)
+	var logConfig zlog.LogsConfig
 
-	fmt.Printf("barOut %v", barOut)
-	// Output:
-	// barOut 7
+	errViperUnmarshal := viper.Unmarshal(&logConfig)
+	if errViperUnmarshal != nil {
+		panic(errViperUnmarshal)
+	}
+
+	errInitLogger := zlog.InitLogger(logConfig)
+	if errInitLogger != nil {
+		panic(errInitLogger)
+	}
+
+	zlog.Log().Info("hello zlog")
 }
